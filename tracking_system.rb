668bad5a2c8ab6fa5b88c1c705652d9454453a0b
stevenpy <stevenphilbert@gmail.com>
2016-10-31 23:04:07 -0400

    edit coding style

--- tracking_system.rb
+++ tracking_system.rb
@@ -7,17 +7,17 @@ end
 
 post '/tracking' do
   TrackingInfo.create(domain: params['domain'],
-  										devicetype: params['device-type'],
-  										region: params['region'],
-  										city: params['city'],
-  										country: params['country'],
-  										clientname: params['client-name'],
-  										clientos: params['client-os'],
-  										url: params['url'],
-  										ip: params['ip'],
-  										recipient: params['recipient'],
-  										timestamp: params['timestamp'],
-  										event: params['event'])
+  	devicetype: params['device-type'],
+  	region: params['region'],
+  	city: params['city'],
+  	country: params['country'],
+  	clientname: params['client-name'],
+  	clientos: params['client-os'],
+  	url: params['url'],
+  	ip: params['ip'],
+  	recipient: params['recipient'],
+  	timestamp: params['timestamp'],
+  	event: params['event'])
 end
 
 get '/' do
